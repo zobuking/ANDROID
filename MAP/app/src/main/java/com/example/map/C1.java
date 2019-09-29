6078da5b6e1267a9c7770e414cba4c28979d2a2c
zobuking <zobuking@gmail.com>
2019-09-30 02:46:31 +0600

    MMM

---
+++ MAP/app/src/main/java/com/example/map/C1.java
@@ -0,0 +1,60 @@
+package com.example.map;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class C1 extends Fragment implements OnMapReadyCallback {
+
+     GoogleMap map;
+    public C1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View v= inflater.inflate(R.layout.fragment_c1, container, false);
+
+
+
+       return  v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+            map=googleMap;
+        LatLng pp =new LatLng(11,104);
+        MarkerOptions option=new MarkerOptions();
+        option.position(pp).title("CURRENT LOCATION");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(pp));
+    }
+}
